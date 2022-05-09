de27109cf54ae9859d464393baff8bdcca599290
Terry <terrybmochire@gmail.com>
2022-05-09 19:22:10 +0300

    Implement RangerDao interface for RangerDao.

---
+++ src/main/java/DB/RangerDao.java
@@ -0,0 +1,37 @@
+package DB;
+
+import models.Animal;
+
+import java.util.List;
+
+public class RangerDao implements RangerInterface{
+    @Override
+    public void add() {
+
+    }
+
+    @Override
+    public void findByID(int id) {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public List<Animal> getAll() {
+        return null;
+    }
+
+    @Override
+    public void deleteById() {
+
+    }
+
+    @Override
+    public void clearAllAnimals() {
+
+    }
+}
---
+++ src/main/java/DB/RangerInterface.java
@@ -0,0 +1,23 @@
+package DB;
+
+import models.Animal;
+
+import java.util.List;
+
+public interface RangerInterface {
+    //CREATE
+    void add();
+
+    //FIND
+    void findByID(int id);
+
+    //UPDATE
+    void update();
+
+    //LIST
+    List<Animal> getAll();
+
+    //DELETE
+    void deleteById();
+    void clearAllAnimals();
+}
