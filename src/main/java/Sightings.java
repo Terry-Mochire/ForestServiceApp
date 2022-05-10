ed32fbee927a18081a58055f65a29262735fd1a7
Terry <terrybmochire@gmail.com>
2022-05-10 14:29:44 +0300

    Save sightings details to database.

--- src/main/java/Sightings.java
+++ src/main/java/Sightings.java
@@ -1,21 +1,22 @@
-
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Objects;
+import org.sql2o.Connection;
 
 public class Sightings {
 
-
+    private int id;
+    private int animal_id;
+    private String rangerName;
     private String location;
 
-    private String rangerName;
+    private Timestamp lastSighting;
 
-    private int animalId;
 
-    private String animalName;
-
-    public Sightings(String rangerName, String location, int animalId, String animalName){
+    public Sightings( int animal_id, String location,  String rangerName){
         this.rangerName = rangerName;
         this.location = location;
-        this.animalId = animalId;
-        this.animalName = animalName;
+        this.animal_id = animal_id;
     }
 
     public String getRangerName() {
@@ -26,7 +27,59 @@ public class Sightings {
         return location;
     }
 
-    public int getAnimalId() {
-        return animalId;
+    public int getAnimal_id() {
+        return animal_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Timestamp getLastSighting() {
+        return lastSighting;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Sightings sightings = (Sightings) o;
+        return animal_id == sightings.animal_id &&
+                Objects.equals(rangerName, sightings.rangerName) &&
+                Objects.equals(location, sightings.location);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(animal_id, rangerName, location);
+    }
+
+    public void save() {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO sightings (animal_id, location, rangername, lastsighting) VALUES (:animal_id, :location, :rangerName, now())";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("animal_id", this.animal_id)
+                    .addParameter("location", this.location)
+                    .addParameter("rangerName", this.rangerName)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+
+    public static List<Sightings> all() {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM sightings";
+            return con.createQuery(sql)
+                    .executeAndFetch(Sightings.class);
+        }
+    }
+
+    public static Sightings find(int id) {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM sightings WHERE id=:id";
+            return con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Sightings.class);
+        }
     }
 }
--- src/test/java/SightingsTest.java
+++ src/test/java/SightingsTest.java
@@ -1,12 +1,20 @@
 
 
+import org.junit.Rule;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SightingsTest {
+
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
 
     @BeforeEach
     void setUp() {
@@ -16,7 +24,7 @@ class SightingsTest {
     void tearDown() {
     }
     public Sightings setUpNewSightings(){
-        return new Sightings("Brad", "Near the River", 1, "Lion");
+        return new Sightings(1, "Near the River", "Brad");
     }
 
     @Test
@@ -39,6 +47,39 @@ class SightingsTest {
     @Test
     void getAnimalId_CorrectlyReturnsAnimalId_true() {
         Sightings testSighting = setUpNewSightings();
-        assertEquals(1, testSighting.getAnimalId());
+        assertEquals(1, testSighting.getAnimal_id());
     }
+
+    @Test
+    void equals_returnsTrueIfNameAnimalIDAndRangerAreEqual(){
+        Sightings testSighting = setUpNewSightings();
+        Sightings testSighting2 = setUpNewSightings();
+        assertTrue(testSighting.equals(testSighting2));
+    }
+
+    @Test
+    void save_CorrectlyInsertsObjectsIntoDatabase(){
+        Sightings testSighting = setUpNewSightings();
+        testSighting.save();
+        assertTrue(Sightings.all().get(0).equals(testSighting));
+    }
+
+    @Test
+    public void save_saveEndangeredIdIntoDatabase_true() {
+        Animal becky = new Animal("Becky");
+        becky.save();
+        Sightings nairobi = new Sightings(becky.getId(), "Zone A", "Becky");
+        nairobi.save();
+        assertEquals(nairobi.getAnimal_id(), becky.getId());
+    }
+
+    @Test
+    public void save_recordsTimeOfSightingInDataBase() {
+        Sightings testSighting = new Sightings(1, "Nairobi", "Becky");
+        testSighting.save();
+        Timestamp savedSighting = Sightings.find(testSighting.getId()).getLastSighting();
+        Timestamp rightNow = new Timestamp(new Date().getTime());
+        assertEquals(DateFormat.getDateTimeInstance().format(rightNow), DateFormat.getDateTimeInstance().format(savedSighting));
+    }
+
 }
